413047bc3ca757153a5e60c25516fe3370687648
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2024-07-31 16:37:11 +0300

    first commit

---
+++ resources/views/frontend/sections/hero.blade.php
@@ -0,0 +1,22 @@
+<!-- Header-Area-Start -->
+<header class="header-area parallax-bg" id="home-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="header-text">
+                    <h3 class="typer-title wow fadeInUp" data-wow-delay="0.2s">I'm ui/ux designer</h3>
+                    <h1 class="title wow fadeInUp" data-wow-delay="0.3s"></h1>
+                    <div class="desc wow fadeInUp" data-wow-delay="0.4s">
+                        <p></p>
+                    </div>
+                    {{-- @if ($hero->btn_text)
+                        <a href="{{ $hero->btn_url }}" class="button-dark mouse-dir wow fadeInUp"
+                            data-wow-delay="0.5s">{{ $hero->btn_text }}<span class="dir-part"></span></a>
+                    @endif --}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header-Area-End -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\Admin\Dashboard\DashboardController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('blog', function () {
+    return view('frontend.blog');
 });
+
+Route::get('blog-details', function () {
+    return view('frontend.blog-details');
+});
+
+Route::get('portfolio-details', function () {
+    return view('frontend.portfolio-details');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
